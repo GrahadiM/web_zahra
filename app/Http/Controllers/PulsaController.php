ea5eac810ff52a173769192d647b7ebef92c7101
unknown <grahadim178@gmail.com>
2021-11-16 02:34:31 +0700

    outlet commit

--- app/Http/Controllers/PulsaController.php
+++ app/Http/Controllers/PulsaController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Model\LogAdmin;
-use Auth;
-use DB;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class PulsaController extends Controller
 {
